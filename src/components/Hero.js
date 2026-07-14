a3846d7559e1acdfd626ae6407bc1d2c3e0e43f5
Munir Uddin Mahbub <67515174+Mahbub-Hasan2@users.noreply.github.com>
2026-07-14 10:15:29 +0300

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -17,7 +17,7 @@ export default function Hero({ product }) {
         "
         style={{
           backgroundImage:
-            "url('https://images.unsplash.com/photo-1503376780353-7e6692767b70?q=80&w=1200')",
+            "url('https://images.pexels.com/photos/193997/pexels-photo-193997.jpeg?_gl=1*1yj2cse*_ga*MTQ5Njc0MzUyMS4xNzg0MDEyOTYz*_ga_8JE65Q40S6*czE3ODQwMTI5NjIkbzEkZzEkdDE3ODQwMTMyMTQkajI3JGwwJGgw')",
         }}
       />
 
